51721f466dd71ffdbf51faefd79737c7e4b9164a
GwonMinji <75905963+choie1346@users.noreply.github.com>
2025-09-30 17:07:45 +0900

    grass Class 구성

--- Falling_Ball.py
+++ Falling_Ball.py
@@ -1,8 +1,16 @@
 from pico2d import *
 import random
 
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
 def reset_world():
-    pass
+    global grass
+    grass = Grass()
 
 
 def update_world():
@@ -10,7 +18,7 @@ def update_world():
 
 def render_world():
     clear_canvas()
-
+    grass.draw()
     update_canvas()
 
 
